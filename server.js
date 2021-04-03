571039fc55e8871776316c28a9f5e55dffb5e7e5
LuisFilipeMendonca <filipedev@outlook.pt>
2021-04-03 15:35:06 +0100

    Fixed some bugs

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import app from "./app";
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
